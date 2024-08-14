9c75b26974841243070119c5fbbf00a51108589c
zeyadfaiyoumi <zeyadfaiyoumi@gmail.com>
2024-08-14 11:24:07 +0300

    elections

--- Back-end/migrations/20240814064258_migration_citizens_table.js
+++ Back-end/migrations/20240814064258_migration_citizens_table.js
@@ -1,23 +1,20 @@
-//جدول كل المواطنين
-
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
 exports.up = function (knex) {
   return knex.schema.createTable("citizens", function (table) {
-    table.increments("id").primary();
-    table.string("national_id").notNullable().unique(); // Added national_id
-    table.string("name").notNullable(); // Name column
-    table.string("religion").notNullable(); // Added religion
-    table.string("email").notNullable().unique(); // Email column
-    table.string("governorate").notNullable(); // Added governorate
-    table.string("electoral_district").notNullable(); // Added electoral_district
-    table.string("region").notNullable(); // Added region
-    table.boolean("is_voted_party").defaultTo(false); // Added is_voted-party
-    table.boolean("is_voted_local").defaultTo(false); // Added is_voted_local
-    table.string("gender").notNullable(); // Added gender
-    table.timestamps(true, true);
+    table.string("national_id").primary(); // جعل national_id هو المفتاح الأساسي
+    table.string("name").notNullable(); // عمود الاسم
+    table.string("religion").notNullable(); // عمود الدين
+    table.string("email").notNullable().unique(); // عمود البريد الإلكتروني
+    table.string("governorate").notNullable(); // عمود المحافظة
+    table.string("electoral_district").notNullable(); // عمود الدائرة الانتخابية
+    table.string("region").notNullable(); // عمود المنطقة
+    table.boolean("is_voted_party").defaultTo(false); // عمود التصويت للحزب
+    table.boolean("is_voted_local").defaultTo(false); // عمود التصويت المحلي
+    table.string("gender").notNullable(); // عمود الجنس
+    table.timestamps(true, true); // الطوابع الزمنية
   });
 };
 
@@ -28,4 +25,3 @@ exports.up = function (knex) {
 exports.down = function (knex) {
   return knex.schema.dropTable("citizens");
 };
-
